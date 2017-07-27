4ed8a483c981e3d8b200f4599efc248d6da98328
jeffrey <jeffrey.vt@hotmail.com>
2017-07-28 08:56:58 +1000

    fixed q1

--- week2/q1_realToBin.py
+++ week2/q1_realToBin.py
@@ -16,17 +16,26 @@ def decimal2bin(num):
     output = ""
     for i in range(10):
         num *= 2
-        if num > 1:
+        if num >= 1:
             output += "1"
             num -= 1
         else:
             output += "0"
+        if abs(num - 0.0) < 0.01:
+            break
     return output
 
 
 def float2bin(num):
-    i, d = divmod(num, 1)
-    result = int2bin(int(i)) + "." + decimal2bin(d)
+    tmp = str(num).split(".")
+    if len(tmp) > 1:
+        i = int(tmp[0])
+        d = float("0."+tmp[1])
+        print(i, d)
+        result = int2bin(i) + "." + decimal2bin(d)
+    else:
+        i = int(tmp[0])
+        result = int2bin(i)
     print(result)
 
-float2bin(10.11)
+float2bin(10)
