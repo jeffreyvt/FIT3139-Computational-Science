e6d9c2f327debc408188bcb6dcba4afc194112dd
jeffrey <jeffrey.vt@hotmail.com>
2017-08-02 19:34:28 +1000

    week2 lab completed

--- week2/q9_coord.py
+++ week2/q9_coord.py
@@ -1,31 +1,36 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def taylor_sine(x, d):
+    """
+    Element-wise function for sine.
+    :param x: sine argument
+    :param d: specifies when to stop the taylor series expansion
+    :return: the taylor expansion of x to d-th term
+    """
     ans = x
     factorial = 1
     for i in range(1, d):
-        factorial *= 2*i*(2*i+1)
-        ans += np.power(-1, 2*i+1 )*np.power(x, 2*i+1)/factorial
+        factorial *= 2 * i * (2 * i + 1)
+        ans += np.power(-1, i) * np.power(x, 2 * i + 1) / factorial
     return ans
+
 
 if __name__ == "__main__":
     x = np.arange(0, np.pi, 0.015)
-    y_real = x-1-0.5*np.sin(x)
-    plt.plot(x,y_real)
-    y_est = x - 1 - 0.5 * taylor_sine(x, 10)
-    plt.plot(x, y_est, "g")
-    plt.show()
-    input()
+    y_real = x - 1 - 0.5 * np.sin(x)
+    y_est = np.zeros(x.size)
 
+    plt.plot(x, y_est, "g*")
 
-
-    i = 1
+    d = 1
     while True:
-        y_est = x-1-0.5*taylor_sine(x, i)
-        print(y_est)
+        for i in range(x.size):
+            y_est[i] = x[i] - 1 - 0.5 * taylor_sine(x[i], d)
         error = np.max(np.abs(y_est - y_real))
-        print(error)
-        i += 1
-        if i == 10:
+        print("Taylor series expansion order: ", d, "max error: ", error)
+        d += 1
+        if d > 12 or error < 0.015:
             break
+    
