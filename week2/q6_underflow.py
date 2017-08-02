f20a8c24591a5acd97f43917ffaff3ed13e6ad87
jeffrey <jeffrey.vt@hotmail.com>
2017-08-02 12:52:11 +1000

    completed q6 overflow and underflow limit of numpy

--- week2/q6_underflow.py
+++ week2/q6_underflow.py
@@ -1,21 +1,21 @@
 import numpy as np
 
-x = 100.0
-while x != np.inf:
+x = -100.0
+while x != -np.inf:
     x_previous = x
     x = np.multiply(x, 2.0)
 x = x_previous
-while x != np.inf:
+while x != -np.inf:
     x_previous = x
     x = np.multiply(x, 1.1)
 
 x = x_previous
-while x != np.inf:
+while x != -np.inf:
     x_previous = x
     x = np.multiply(x, 1.01)
 
 x = x_previous
-while x != np.inf:
+while x != -np.inf:
     x_previous = x
     x = np.multiply(x, 1.001)
 print(x_previous)
