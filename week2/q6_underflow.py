9291582fd84afafd0f3d86368e94595d04e2faa0
jeffrey <jeffrey.vt@hotmail.com>
2017-08-02 14:21:27 +1000

    fixed q6 for more accurate estimates

--- week2/q6_underflow.py
+++ week2/q6_underflow.py
@@ -1,21 +1,13 @@
 import numpy as np
 
 x = -100.0
-while x != -np.inf:
+multiplier = 2.0
+while True:
     x_previous = x
-    x = np.multiply(x, 2.0)
-x = x_previous
-while x != -np.inf:
-    x_previous = x
-    x = np.multiply(x, 1.1)
-
-x = x_previous
-while x != -np.inf:
-    x_previous = x
-    x = np.multiply(x, 1.01)
-
-x = x_previous
-while x != -np.inf:
-    x_previous = x
-    x = np.multiply(x, 1.001)
-print(x_previous)
+    x = np.multiply(x, multiplier)
+    if x == -np.inf:
+        x = x_previous
+        multiplier = (multiplier - 1.0) / 10.0 + 1.0
+    if multiplier < 1.00000001:
+        break
+print(x)
