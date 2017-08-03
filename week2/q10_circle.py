43424665edb58fc64645274542e86b252d4a841b
jeffrey <jeffrey.vt@hotmail.com>
2017-08-03 15:42:58 +1000

    minor fix on q10

--- week2/q10_circle.py
+++ week2/q10_circle.py
@@ -1,15 +1,22 @@
 import numpy as np
 
 
-def est_area(r, dx=1):
+def est_area(r, dx=1.0):
     x = np.arange(0, r, dx)
     y = np.sqrt(r * r - np.power(x, 2))
     y = np.floor(y / dx) * dx
-    return (np.sum(y)-y[0]) * dx * 4
+    return (np.sum(y) - y[0]) * dx * 4
 
 
 if __name__ == "__main__":
-    r = 100
+    r = 10
     estimated_area = est_area(r)
     estimated_pi = estimated_area / (r * r)
-    print("estimated pi is = ", estimated_pi, "The absolute error is: ", abs(np.pi - estimated_pi))
+    print("r =", r, "dx = 1", "estimated pi =", estimated_pi, "The absolute error is:", np.abs(np.pi - estimated_pi))
+
+    r = 100
+    dx = 0.01
+    estimated_area = est_area(r, dx=dx)
+    estimated_pi = estimated_area / (r * r)
+    print("r =", r, "dx =", dx, "estimated pi =", estimated_pi, "The absolute error is:",
+          np.abs(np.pi - estimated_pi))
